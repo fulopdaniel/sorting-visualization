1e9d1d959e66488beff5b724d31c4146e2f02634
Fülöp Dániel <daniel@Fulop-MacBook-Pro.local>
2020-09-12 16:37:50 +0200

    Updates

---
+++ precache-manifest.55ff0563cfdf80894ff6d6d86af5e7da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bbccd2fb42d023ce11ca5a8654fbe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91f2233d900da829ffc4",
+    "url": "/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "9d43300a56deb1a59dbd",
+    "url": "/static/css/main.4ceec2cc.chunk.css"
+  },
+  {
+    "revision": "91f2233d900da829ffc4",
+    "url": "/static/js/2.2f071180.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.2f071180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d43300a56deb1a59dbd",
+    "url": "/static/js/main.6302e783.chunk.js"
+  },
+  {
+    "revision": "a0003e65cf28c585feb8",
+    "url": "/static/js/runtime-main.6ecb8fcc.js"
+  }
+]);
